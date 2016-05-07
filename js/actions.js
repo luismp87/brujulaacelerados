ec7da7854e8bc826802e73524ec4afdce4667348
luismp87 <luismp87@gmail.com>
2016-05-07 11:35:37 -0500

    ss

--- js/actions.js
+++ js/actions.js
@@ -8,9 +8,10 @@ var fn = {
     ready: function(){
 document.addEventListener("deviceready",fn.init,false);
         
-    }
+    },
     init: function(){
-    $('acelerometro .individual')
-    $('acelerometro .individual')    
+    $('acelerometro .individual li').eq(0).tap(ac.iniciar);
+    $('acelerometro .individual li').eq(1).tap(ac.iniciar);    
 }
-}
+};
+$(fn.ready);
