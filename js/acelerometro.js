d5e9fdbbf255a7c2ad64ea87ea3c875fecb808fa
luismp87 <luismp87@gmail.com>
2016-05-07 11:29:58 -0500

    ss

---
+++ js/acelerometro.js
@@ -0,0 +1,24 @@
+var ac = {
+    watchID: null,
+    iniciar:function(){
+       if(!ac.watchID){
+           var options  = {frequency: 500};
+           ac.watchID = navigator.accelerometro.watchAcceleration(ac.verDatos, ac.error,options);
+        } 
+    },
+    detener: function(){
+        if(ac.watchID){
+            navigator.accelerometer.clearWatch(ac.watchID);
+        }
+        
+    },
+    error: function(err){
+        alert(err.code);
+        
+    },
+    verDatos: function(a){
+        $('#acelerometro h2').html('x: ' + a.x + '<br>' + 'y: ' + a.y + '<br>' + 'z: ' + a.z);
+        
+    }
+    
+};
---
+++ js/actions.js
@@ -0,0 +1,16 @@
+//actions
+var jQT = new $.jQTouch({
+	themeSelectionSelector: '#jqt'
+});
+
+var fn = {
+    
+    ready: function(){
+document.addEventListener("deviceready",fn.init,false);
+        
+    }
+    init: function(){
+    $('acelerometro .individual')
+    $('acelerometro .individual')    
+}
+}
